090d9b8dd3c0b8ef8064589e27220be55664212a
Drygo <davidgp2b@outlook.com>
2025-03-29 14:16:23 -0700

    🔧 Fixed Build Misstakes.

--- src/main/java/org/eldrygo/XWhitelist.java
+++ src/main/java/org/eldrygo/XWhitelist.java
@@ -5,6 +5,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.eldrygo.API.XWhitelistAPI;
 import org.eldrygo.Managers.ConfigManager;
+import org.eldrygo.Managers.FileWhitelistManager;
+import org.eldrygo.Managers.MWhitelistManager;
+import org.eldrygo.Managers.MySQLWhitelistManager;
 import org.eldrygo.Utils.ChatUtils;
 import org.eldrygo.Utils.DBUtils;
 import org.eldrygo.Utils.LoadUtils;
@@ -13,45 +16,35 @@ import org.eldrygo.Utils.LogsUtils;
 import java.sql.*;
 import java.util.logging.Logger;
 
-public class XWhitelist extends JavaPlugin implements Listener {
+public class XWhitelist extends JavaPlugin {
 
     public Connection connection;
-    public String version = getDescription().getVersion();
+    public String version;
     public Logger log;
     public FileConfiguration config;
     public boolean workingPlaceholderAPI = false;
     public boolean useMySQL;
     private LogsUtils logsUtils;
     private ConfigManager configManager;
-    private final LoadUtils loadUtils;
-    private final DBUtils dBUtils;
-    private XWhitelistAPI xWhitelistAPI;
-    private final ChatUtils chatUtils;
-
-    public XWhitelist(LoadUtils loadUtils, DBUtils dBUtils, XWhitelistAPI xWhitelistAPI, ChatUtils chatUtils) {
-        this.loadUtils = loadUtils;
-        this.dBUtils = dBUtils;
-        this.xWhitelistAPI = xWhitelistAPI;
-        this.chatUtils = chatUtils;
-    }
-    public boolean isMySQLEnabled() {
-        return useMySQL;
-    }
-    public boolean isPlaceholderAPIEnabled() {
-        return workingPlaceholderAPI;
-    }
-    public Connection getConnection() {
-        return connection;
-    }
-    public String getPrefix() {return configManager.prefix;}
+    private DBUtils dBUtils;
+    private MWhitelistManager mWhitelistManager;
+    private FileWhitelistManager fileWhitelistManager;
+    private MySQLWhitelistManager mySQLWhitelistManager;
 
     @Override
     public void onEnable() {
         log = this.getLogger();
-        this.useMySQL = config.getBoolean("mysql.enable", false);
+        version = getDescription().getVersion();
+        config = getConfig();
+        useMySQL = config.getBoolean("mysql.enable", false);
+
+        ChatUtils chatUtils = new ChatUtils(configManager, this);
         this.configManager = new ConfigManager(this, configManager, chatUtils);
         this.logsUtils = new LogsUtils(this);
-        this.xWhitelistAPI = new XWhitelistAPI(this, connection, configManager);
+        this.dBUtils = new DBUtils();
+        LoadUtils loadUtils = new LoadUtils(configManager, this, mWhitelistManager, dBUtils, fileWhitelistManager, mySQLWhitelistManager, chatUtils);
+        XWhitelistAPI xWhitelistAPI = new XWhitelistAPI(this, connection, configManager);
+
         loadUtils.loadFeatures();
         logsUtils.sendRunMessage();
         logsUtils.sendStartupMessage();
@@ -62,4 +55,8 @@ public class XWhitelist extends JavaPlugin implements Listener {
         dBUtils.unloadDatabase();
         logsUtils.sendShutdownMessage();
     }
+    public boolean isMySQLEnabled() { return useMySQL; }
+    public boolean isPlaceholderAPIEnabled() { return workingPlaceholderAPI; }
+    public Connection getConnection() { return connection; }
+    public String getPrefix() {return configManager.prefix;}
 }
